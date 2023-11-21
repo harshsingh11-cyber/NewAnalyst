a9baca44740e4d30b1f72f88327b3dad5ba9800e
Harsh Singh Chauhan <hchouhan689@gmail.com>
2023-11-21 23:19:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -14,3 +14,31 @@ function App() {
 }
 
 export default App;
+
+
+// Inside the Analyst1 component
+return (
+  <MaterialReactTable
+    table={table}
+    className="custom-material-table"
+    style={{ backgroundColor: '#151b33', color: 'white', fontSize: '16px' }}
+  />
+);
+
+/* Inside Analyst1.css */
+
+.custom-material-table {
+  /* Add your custom styles here */
+  border: 1px solid #ccc;
+  /* Example styles, customize as needed */
+}
+
+.custom-material-table th {
+  background-color: #333;
+  color: white;
+  /* Example styles for table headers, customize as needed */
+}
+
+.custom-material-table td {
+  /* Example styles for table cells, customize as needed */
+}
